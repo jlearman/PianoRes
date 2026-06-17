2c1ea67879d8fef5a1fc2adbf91c442a9d4e18d1
Jeff Learman <jjlearman@gmail.com>
2026-06-16 20:07:21 -0400

    make Release default build target
    needed for Zyhthian build using LinuxMakefiles.

--- mkrelease.sh
+++ mkrelease.sh
@@ -3,22 +3,43 @@
 # After building artifacts using Visual Studio, build a zip file
 # in Artifacts folder, for associating with a release tag.
 
+# After building Zynthian, copy PianoRes.lv2 to Artifacts folder,
+# and run with -z option.
+
+ZYN=false
+if [[ $1 == "-z" ]] ; then
+    ZYN=true
+    shift
+fi
+
 if [ $# -lt 1 ] ; then
     echo "expecting version number, with v prefix"
     exit 1
 fi
 
+
 VERSION=$1
 
-ZIP=PianoRes-$VERSION-win-x64-vst3.zip
+if $ZYN ; then
+    FORMAT=lv2
+    ARCH=zyn-$FORMAT
+    BUILDDIR=Artifacts
+else
+    FORMAT=vst3
+    ARCH=win-x64-$FORMAT
+    BUILDDIR=Builds/VisualStudio2026/x64/Release/VST3/
+fi
+
+ZIP=PianoRes-$VERSION-$ARCH.zip
+
 set -ex
 mkdir -p Artifacts
 rm -f Artifacts/$ZIP
 zip -r Artifacts/$ZIP ImpulseFiles
 
-cd Builds/VisualStudio2026/x64/Release/VST3/
-zip -r $OLDPWD/Artifacts/$ZIP PianoRes.vst3
+cd $BUILDDIR
+zip -r $OLDPWD/Artifacts/$ZIP PianoRes.$FORMAT
 cd -
 
 cd release-readme
-zip -r $OLDPWD/Artifacts/$ZIP README-win-x64-vst3.txt
+zip -r $OLDPWD/Artifacts/$ZIP README-$ARCH.txt
---
+++ release-readme/README-zyn-lv2.txt
@@ -0,0 +1,14 @@
+PianoRes installation:
+
+Copy PianoRes-zyn-lv2.zip/PianoRes.lv2 folder into zynthian lv2 plugin
+folder, which is usually /zynthian/zynthian-plugins/lv2 .
+Using Webconf, go to SOFTWARE -> Engines and click "Scan for Presets."
+Then restart Zynthian.
+
+Alternatively, go to SOFTWARE -> Engines and click "Install Plugin."
+
+Loading of impulse files is not yet supported, due to Zynthian limitations.
+Maybe someday.  PianoRes already includes the
+short impulse file for Accurate Salamander Grand 6.2beta2 and uses it by
+default, without copying the impulse files that happen to be
+included in the zip file.
